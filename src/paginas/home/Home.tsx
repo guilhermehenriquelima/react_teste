3223637785c643ca9a6f831c0e21965e386e99e4
Guilherme Henrique <guilhermehenriquelima02@gmail.com>
2023-05-11 22:57:02 -0300

    React Teste

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,17 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+
+            <h1 className='titulo'>Hello World React</h1>
+            <img className='imagem' src="https://www.infoescola.com/wp-content/uploads/2008/05/cavalo.jpg" alt=""></img>
+
+        </>
+
+    )
+}
+/*  <> (fragment) = significa a criação de um container de modo mais simplificado 
+    para que outros elementos sejam adicionados   */
+
+export default Home;
